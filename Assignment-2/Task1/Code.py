9edc85446c634cf91c016f70153853ee4c26d1a8
Az El Arab Chakour <azelarab.chakour@gmail.com>
2026-05-24 20:04:07 +0200

    task1(c) done

--- Assignment-2/Task1/Code.py
+++ Assignment-2/Task1/Code.py
@@ -18,7 +18,6 @@ print(result)
 random_list = []
 for _ in range(40000):
     random_list.append(random.randint(1, 100))
-
 random_rdd = sc.parallelize(random_list, 10)
 
 def get_bucket(number):
@@ -27,16 +26,26 @@ def get_bucket(number):
     return f"{lower_bound}-{upper_bound}"
 
 bucket_counts_rdd = random_rdd.map(lambda x: (get_bucket(x), 1)).reduceByKey(lambda a, b: a + b)
-
 final_buckets = bucket_counts_rdd.collect()
 final_buckets.sort(key=lambda x: int(x[0].split('-')[0]))
-
 for result in final_buckets:
     print(result)
 #----------------------------------------------------------------
 
 #--------------------- Task 1(c)---------------------------------
-
+"""
+- The parallelize() function is a PySpark function that takes a 
+local Python collection to create an RDD.
+- Resilient Distributed Dataset (RDD) is collection of data objects that can be manipulated in parallel, 
+Resilient(can recover from node failures automatically), Distributed(data is spread across multiple workers), 
+Dataset(it's a read-only collection of records).
+- In Task1(a) we create a list with numbers ranging from 0 to 80000, then we split that list across 
+8 different partitions of an RDD using parallelize() function, then we register the lambda function to filter for just the 
+even numbers but it won't compute anything until we use .take(7) to get the first 7 results.
+- The difference between Python filter() and RDD's filter() is that the Python function executes in linear and 
+directly. In contrast, RDD's function executes in parallel and lazy. Thus, the RDD's filter() can scale horizantally
+unlike the Python's filter() that is limited by vertical scalling.
+"""
 #----------------------------------------------------------------
 
 #--------------------- Task 1(d)---------------------------------
