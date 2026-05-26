99fbb386f19aac71fcf0abb4caf473afb810459c
Az El Arab Chakour <azelarab.chakour@gmail.com>
2026-05-26 18:24:01 +0200

    task1(d) dene

--- Assignment-2/Task1/Code.py
+++ Assignment-2/Task1/Code.py
@@ -1,5 +1,6 @@
 from pyspark import SparkContext, SparkConf
 import random
+from pyspark.ml.feature import StopWordsRemover
 
 #------------------ Intialize Spark Context ----------------------
 conf = SparkConf().setAppName("Task1").setMaster("local[*]")
@@ -7,30 +8,36 @@ sc = SparkContext(conf=conf)
 #----------------------------------------------------------------
 
 #--------------------- Task 1(a)---------------------------------
-list = list(range(0, 80001)) 
-rdd = sc.parallelize(list, 8)
-even_rdd = rdd.filter(lambda x: x % 2 == 0) 
-result = even_rdd.take(7) 
-print(result)
+def task1a():
+    numbers_list = list(range(0, 80001)) 
+    rdd = sc.parallelize(numbers_list, 8)
+    even_rdd = rdd.filter(lambda x: x % 2 == 0) 
+    result = even_rdd.take(7) 
+    print(result)
+    sc.stop()
 #----------------------------------------------------------------
+
 
 #--------------------- Task 1(b)---------------------------------
-random_list = []
-for _ in range(40000):
-    random_list.append(random.randint(1, 100))
-random_rdd = sc.parallelize(random_list, 10)
+def task1b():
+    random_list = []
+    for _ in range(40000):
+        random_list.append(random.randint(1, 100))
+    random_rdd = sc.parallelize(random_list, 10)
 
-def get_bucket(number):
-    lower_bound = ((number - 1) // 10) * 10 + 1
-    upper_bound = lower_bound + 9
-    return f"{lower_bound}-{upper_bound}"
+    def get_bucket(number):
+        lower_bound = ((number - 1) // 10) * 10 + 1
+        upper_bound = lower_bound + 9
+        return f"{lower_bound}-{upper_bound}"
 
-bucket_counts_rdd = random_rdd.map(lambda x: (get_bucket(x), 1)).reduceByKey(lambda a, b: a + b)
-final_buckets = bucket_counts_rdd.collect()
-final_buckets.sort(key=lambda x: int(x[0].split('-')[0]))
-for result in final_buckets:
-    print(result)
+    bucket_counts_rdd = random_rdd.map(lambda x: (get_bucket(x), 1)).reduceByKey(lambda a, b: a + b)
+    final_buckets = bucket_counts_rdd.collect()
+    final_buckets.sort(key=lambda x: int(x[0].split('-')[0]))
+    for result in final_buckets:
+        print(result)
+    sc.stop()
 #----------------------------------------------------------------
+
 
 #--------------------- Task 1(c)---------------------------------
 """
@@ -48,14 +55,30 @@ unlike the Python's filter() that is limited by vertical scalling.
 """
 #----------------------------------------------------------------
 
-#--------------------- Task 1(d)---------------------------------
 
+#--------------------- Task 1(d)---------------------------------
+def task1d():
+    rdd = sc.textFile("Text-data.txt")
+
+    def clean_word(word):
+        cleaned = ''.join(char for char in word if char.isprintable() and char.isalpha())
+        return cleaned.lower()
+
+    words_list = rdd.flatMap(lambda line: line.split()).map(clean_word).filter(lambda word: len(word) > 0)
+    word_counts = words_list.map(lambda word : (word,1)).reduceByKey(lambda a, b: a + b)
+    top_25_words = word_counts.takeOrdered(25, key=lambda x: -x[1])
+    for result in top_25_words:
+        print(result)
 #----------------------------------------------------------------
+
 
 #--------------------- Task 1(e)---------------------------------
 
 #----------------------------------------------------------------
 
-#------------------ Stop Spark Context --------------------------
-sc.stop()
+
+#------------------ Execution -----------------------------------
+#task1a()
+#task1b()
+#task1d()
 #----------------------------------------------------------------
